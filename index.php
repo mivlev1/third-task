d0c24e56631cdfd73c5ae87341e9f0bf53272590
mivlev1 <makzoyn@gmail.com>
2022-04-27 15:39:13 +0300

    edit proj16

--- index.php
+++ index.php
@@ -62,12 +62,12 @@ $db = new PDO('mysql:host=localhost;dbname=u41731', $user, $pass, array(PDO::ATT
 // Подготовленный запрос. Не именованные метки.
 try{
     $stmt = $db->prepare("INSERT INTO application SET name = ?, email=?, dat=?, gender=?, kon=?, checkz=?");
-
     $stmt -> execute([$_POST['name-field'], $_POST['email-field'], date('Y-m-d', strtotime($_POST['date'])), $_POST['radio-gender'], $_POST['radio-kon'], $_POST['checkz']]);
     //$stmt -> execute([$id,$name, $email, $dat, $gender, $kon, $checkz]);
-    $stmt2 = $db->prepare("INSERT INTO power SET power = ?");
-    $stmt2 -> execute([$power]);
     $id = $db->lastInsertId();
+    $stmt2 = $db->prepare("INSERT INTO id,power SET power = ?");
+    $stmt2 -> execute([$id,$power]);
+
     echo "Запись добавлена под номером:" . $id;
 }
 catch(PDOException $e) {
